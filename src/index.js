0ff41265cbd3e63e9aa8a5370db160f10bea9d75
Morgan Collado <morgan.collado@gmail.com>
2021-02-02 18:24:48 -0600

    commit tree ca2998b902654cc7b59b95523e152bfba5bacdd2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 // write your createStore function here
 
+function createStore(reducer) {
+  let state;
+
+  function dispatch(action) {
+    state = reducer(state, action)
+    render()
+  }
+  function getState() {
+    return state
+  }
+  return {
+    dispatch,
+    getState
+  }
+
+}
+
 function candyReducer(state = [], action) {
   switch (action.type) {
     case 'ADD_CANDY':
@@ -8,6 +25,9 @@ function candyReducer(state = [], action) {
       return state;
   }
 }
+
+let store = createStore(candyReducer)
+store.dispatch({type: '@@INIT'})
 
 function render() {
   let container = document.getElementById('container');
